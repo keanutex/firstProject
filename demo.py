1a61ffd7db8e7b752a860092ed53733523a5227c
rawoott <37331566+rawoott@users.noreply.github.com>
2018-05-11 11:27:50 +0200

    PCA graph with selective coloumn choices

--- demo.py
+++ demo.py
@@ -3,89 +3,182 @@ import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 
 
+class PCAPlotter():
+    def __init__(self):
+        self.myFile1 = None
+        self.myFile2 = None
+        self.x = []
+        self.col1 = []
+        self.col2 = []
+        self.col3 = []
+        self.color = []
+        self.a = []
+        self.b = []
+        self.bcol = []
+        self.coloumnPhe = []
+        self.coloumnEvec = []
 
-#-----------------------------------------------------------------------------------------------
+    def readFile1(self, fileName1):
+        with open (fileName1) as self.myFile1:                #Open and read into comm(PCA)file
+            lines = self.myFile1.read().splitlines()              #Return list with all lines in a string
 
-#First File
 
-with open ('comm.phe') as myfile:                #Open and read into comm(PCA)file
- lines = myfile.read().splitlines()              #Return list with all lines in a string
+        emptyList = []
+        s = lines[0]
+        t = s.split()
+        k = len(t)
+        for l in range(k):
+            self.coloumnPhe.append(emptyList)
 
-a = []                                          #Create empty list
-b = []
+        # for i in range (len(lines)):
+        #     s = lines[i]                                #Returns lines in list
+        #     t = s.split()                               #Splits lines into columns
+        #
+        #     #k=len(t)
+        #     #listVals.append(t)
+        #
+        #     self.a.append(t[0])                              #Adds 0th column into list
+        #     self.b.append(t[3])
+        #     self.bcol.append(t[2])
 
-for i in range (len(lines)):
-    s = lines[i]                                #Returns lines in list
-    t = s.split()                               #Splits lines into columns
-    a.append(t[0])                              #Adds 0th column into list
-    b.append(t[3])
 
+
+        for m in range(k):
+            vals =[]
+            for i in range(len(lines)):
+                s = lines[i]
+                t = s.split()
+                vals.append(t[m])
+
+            self.coloumnPhe[m] = vals
+
+        #print(self.coloumnPhe[0])
 
 
 #-------------------------------------------------------------------------------------------
 
 #Second File
+    def readFile2(self, fileName2):
 
-with open('comm-SYMCL.pca.evec') as myFile:
-    next(myFile)                                #Skip over first line of file
-    list = myFile.read().splitlines()
-
-x = []
-col1 = []
-col2 = []
-col3 = []
-col4 = []
+        with open(fileName2) as myFile2:
+            next(myFile2)                                #Skip over first line of file
+            lines = myFile2.read().splitlines()          #lines are the list of lines
 
 
-for i in range(len(list)):
-    s = list[i]
-    m = s.replace(":", "\t")          #Replace any colon found with a tab, to seperate columns
-    t = m.split()
-    x.append((t[0]))
-    col1.append(float(t[2]))
-    col2.append(float(t[3]))
-    col3.append(float(t[4]))
+        for i in range(len(lines)):
+            s = lines[i]
+            m = s.replace(":", "\t")          #Replace any colon found with a tab, to seperate columns
+            lines[i] = m
+            t = m.split()
+            self.x.append((t[0]))
+            # self.col1.append(float(t[2]))
+            # self.col2.append(float(t[3]))
+            # self.col3.append(float(t[4]))
+
+        emptyList = []                                 #creating the coloumns
+        s = lines[0]
+        t = s.split()
+        k = len(t)
+        notAllowedColoums = 0
+        for l in range(k):
+            self.coloumnEvec.append(emptyList)
+
+        for m in range(0,k):
+            vals =[]
+            isNum = True
+            for i in range(len(lines)):
+                s = lines[i]
+                t = s.split()
+
+                try:
+                    vals.append(float(t[m]))
+                except ValueError:
+                    isNum = False
+                    pass
+
+            if (isNum == True):
+                self.coloumnEvec[m] = vals
+            else:
+                notAllowedColoums= notAllowedColoums + 1
+
+        for n in range(notAllowedColoums):
+            self.coloumnEvec.remove([])
 
 
-print (x)
+
 
 
 #-------------------------------------------------------------------------------------
 
-# Connect files and add color
+# Connect files and add self.color
+    def connectFilesAddColour(self,evecCol1,evecCol2,evecCol3,pheCol):
+
+        index = [i for i, item in enumerate(self.x) if item in self.coloumnPhe[0]]
+        T = [self.x[i] for i in index] # gives only the values indexed to right eigenvalue
+        for j in range(len(self.coloumnEvec)):
+            self.coloumnEvec[j] = [self.coloumnEvec[j][i] for i in index]
 
 
-index = [i for i, item in enumerate(x) if item in a]
-T = [x[i] for i in index] # gives only the values indexed to right eigenvalue
-col1 = [col1[i] for i in index]
-col2 = [col2[i] for i in index]
-col3 = [col3[i] for i in index]
+        if (len(self.coloumnEvec) < 3):
+            emptylist = []
+            for j in range(len(self.coloumnEvec[0])):
+                 emptylist.append(0)
+            self.coloumnEvec.append(emptylist)
 
-print (col1)
+        # col1 = [self.col1[i] for i in index]
+        # col2 = [self.col2[i] for i in index]
+        # col3 = [self.col3[i] for i in index]
 
+        self.col1 = self.coloumnEvec[evecCol1]
+        self.col2 = self.coloumnEvec[evecCol2]
+        self.col3 = self.coloumnEvec[evecCol3]
+        catagoryCol = self.coloumnPhe[pheCol]
 
-color = []
-for position,string in enumerate(b):
-        if string=='EUR':
-            color.append('#ff0000')
-        if string == 'ASN':
-            color.append('#00ff00')
-        if string == 'AFR':
-            color.append('#0000ff')
+        index2 = [n for n, item in enumerate(self.x) if item in self.coloumnPhe[0]]
+        T2 = [self.x[n] for n in index]
+        catagoryCol = [catagoryCol[n] for n in index]
 
-color = [color[i] for i in index]
+        #print(self.bcol)
+
+        colorHex = ['#C0C0C0','#FF0000','#FFFF00','#00FF00','#008000','#00FFFF','#008080','#0000FF','#FF00FF','#800080','#008000','#000000']
+
+        catagory = []
+        catagory.append(catagoryCol[0])
+        for position,string in enumerate(catagoryCol):
+            check= False
+            max = 0
+            for i in range(0,len(catagory)):
+                if string==catagory[i]:
+                    self.color.append(colorHex[i])
+                    check = True
+                    max = i
+            if check==False:
+                catagory.append(string)
+                self.color.append(colorHex[max+1])
+        '''
+        for position,string in enumerate(self.b):
+                if string=='EUR':
+                    self.color.append('#ff0000')
+                if string == 'ASN':
+                    self.color.append('#00ff00')
+                if string == 'AFR':
+                    self.color.append('#0000ff')
+
+        self.color = [self.color[i] for i in index]'''
 #-------------------------------------------------------------------------------------------------------
-
+    def plotGraph(self):
 #Plot as graph
+        fig = plt.figure()
+        ax = plt.axes(projection='3d')
 
-fig = plt.figure()
-ax = plt.axes(projection='3d')
+        ax.scatter3D(self.col1, self.col2, self.col3,s=1,c=self.color);
+        ax.set_xlabel('X Label')
+        ax.set_ylabel('Y Label')
+        ax.set_zlabel('Z Label')
+        plt.show()
 
-ax.scatter3D(col1, col2, col3,s=1,c=color);
-ax.set_xlabel('X Label')
-ax.set_ylabel('Y Label')
-ax.set_zlabel('Z Label')
-plt.show()
-
-
-
+plotter = PCAPlotter()
+plotter.readFile1('comm.phe')
+plotter.readFile2('comm-SYMCL.pca.evec')
+plotter.connectFilesAddColour(1,2,4,2)
+plotter.plotGraph()
